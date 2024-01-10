3e176b64dea6aa0d9757c611e44c1b40a316e575
abrahamgreyson <82011220@qq.com>
2024-01-10 18:40:04 +0800

    test: service provider

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -1,3 +1,21 @@
 <?php
 
 namespace Abe\ChineseRegionsForLaravel\Tests;
+
+test('service provider is discovered', function () {
+    $providers = config('app.providers');
+    expect($providers)->toContain('Abe\ChineseRegionsForLaravel\ChineseRegionsServiceProvider');
+});
+
+test('commands are registered', function () {
+    $commands = app('Illuminate\Contracts\Console\Kernel')->all();
+    expect($commands)->toHaveKey('chinese-regions:import');
+});
+
+test('migration is registered', function () {
+    $path = app('migrator')->paths();
+    $names = array_map(function ($path) {
+        return basename($path);
+    }, $path);
+    expect($names)->toContain('create_chinese_regions_table.php');
+});
