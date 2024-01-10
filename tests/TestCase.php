09a4b692f85570d7fadc37236b32f6251aaf575a
abrahamgreyson <82011220@qq.com>
2024-01-11 02:40:44 +0800

    fix: test in testbench 7

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,14 @@
 
 namespace Abe\ChineseRegionsForLaravel\Tests;
 
+use Abe\ChineseRegionsForLaravel\ChineseRegionsServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    use RefreshDatabase, WithWorkbench;
+    use RefreshDatabase;
 
     public function defineEnvironment($app): void
     {
@@ -20,8 +21,22 @@ class TestCase extends Orchestra
         ]);
     }
 
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            ChineseRegionsServiceProvider::class
+        ];
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 }
