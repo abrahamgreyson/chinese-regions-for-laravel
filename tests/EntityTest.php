e79defcc8ab68df16854806013ce333b0ce5a86d
abrahamgreyson <82011220@qq.com>
2024-01-10 17:58:15 +0800

    test: test model

--- tests/EntityTest.php
+++ tests/EntityTest.php
@@ -19,20 +19,27 @@ test('can get all provinces', function () {
     $this->assertEquals('黑龙江省', $provinces[2]->name);
 });
 
-test('province do not have parent', function () {
-    $chineseRegion = new ChineseRegion();
-    $province = $chineseRegion->provinces->first();
-    expect(($province))->toBeInstanceOf(ChineseRegion::class);
-    $this->assertNull($province->parent);
-});
-
 test('can get cities', function () {
     $chineseRegion = new ChineseRegion();
     $cities = $chineseRegion->cities->toArray();
     expect($cities)->toBeEmpty();
-    $province = $chineseRegion->where('code', 1)->orderBy('code', 'desc')->first();
+    $province = $chineseRegion->where('level', 1)->orderBy('code', 'desc')->first();
     $cities = $province->cities;
     $this->assertCount(3, $cities);
+});
+
+test('can get areas', function () {
+    $chineseRegion = new ChineseRegion();
+    $city = $chineseRegion->where('code', 2303)->first();
+    $areas = $city->areas;
+    expect($areas)->toHaveCount(2);
+});
+
+test('province has no parent', function () {
+    $chineseRegion = new ChineseRegion();
+    $province = $chineseRegion->provinces->first();
+    expect(($province))->toBeInstanceOf(ChineseRegion::class);
+    $this->assertNull($province->parent);
 });
 
 test('city has parent', function () {
@@ -43,20 +50,13 @@ test('city has parent', function () {
     $this->assertEquals($province->code, $city->parent->code);
 });
 
-test('can get areas', function () {
-    $chineseRegion = new ChineseRegion();
-    $jixi = $chineseRegion->where('code', 2303)->first();
-    $areas = $jixi->areas;
-    expect($areas)->toHaveCount(2);
-});
-
 test('can get streets', function () {
     $chineseRegion = new ChineseRegion();
     $jixi = $chineseRegion->where('code', 2303)->first();
     $area = $jixi->areas->first();
     $streets = $area->streets;
-    expect($streets)->toHaveCount(2);
-    expect($streets->pluck('name')->toArray())->toMatchArray(['连珠山镇', '黑台镇']);
+    expect($streets)->toHaveCount(2)
+        ->and($streets->pluck('name')->toArray())->toMatchArray(['连珠山镇', '黑台镇']);
 });
 
 test('can get villages', function () {
@@ -66,12 +66,17 @@ test('can get villages', function () {
 });
 
 test('can get children', function () {
-    // todo
     $street = ChineseRegion::where('code', 230382101)->first();
-    dd($street->children);
+    expect($street->children)->toHaveCount(2)
+    ->and($street->children->pluck('name')->toArray())->toEqualCanonicalizing(['永泉村委会', '连珠山村委会']);
 });
 
-test('village does not have children', function () {
+test('village has no children', function () {
     $village = ChineseRegion::where('code', 230382101205)->first();
-    dd($village->children);
+    expect($village->children)->toBeEmpty();
+});
+
+test('last level has no children', function () {
+    $city = ChineseRegion::where('code', 230321)->first();
+    expect($city->children)->toBeEmpty();
 });
--- workbench/database/seeders/ChineseRegionsTestSeeder.php
+++ workbench/database/seeders/ChineseRegionsTestSeeder.php
@@ -16,23 +16,23 @@ class ChineseRegionsTestSeeder extends Seeder
     {
         DB::table('chinese_regions')->insert([
             // province level
-            ['name' => '黑龙江省', 'code' => 23, 'level' => 1, 'province_code' => null, 'city_code' => null, 'area_code' => null, 'street_code' => null],
-            ['name' => '北京市', 'code' => 11, 'level' => 1, 'province_code' => null, 'city_code' => null, 'area_code' => null, 'street_code' => null],
-            ['name' => '辽宁省', 'code' => 21, 'level' => 1,  'province_code' => null, 'city_code' => null, 'area_code' => null, 'street_code' => null],
+            ['name' => '黑龙江省', 'code' => 23, 'level' => 1, 'province_code' => null, 'city_code' => null, 'area_code' => null, 'street_code' => null, 'parent_code' => null],
+            ['name' => '北京市', 'code' => 11, 'level' => 1, 'province_code' => null, 'city_code' => null, 'area_code' => null, 'street_code' => null, 'parent_code' => null],
+            ['name' => '辽宁省', 'code' => 21, 'level' => 1,  'province_code' => null, 'city_code' => null, 'area_code' => null, 'street_code' => null, 'parent_code' => null],
             // city level
-            ['name' => '鸡西市', 'code' => 2303, 'level' => 2, 'province_code' => 23, 'city_code' => null, 'area_code' => null, 'street_code' => null],
-            ['name' => '鹤岗市', 'code' => 2304, 'level' => 2, 'province_code' => 23, 'city_code' => null, 'area_code' => null, 'street_code' => null],
-            ['name' => '黑河市', 'code' => 2311, 'level' => 2, 'province_code' => 23, 'city_code' => null, 'area_code' => null, 'street_code' => null],
+            ['name' => '鸡西市', 'code' => 2303, 'level' => 2, 'province_code' => 23, 'city_code' => null, 'area_code' => null, 'street_code' => null, 'parent_code' => 23],
+            ['name' => '鹤岗市', 'code' => 2304, 'level' => 2, 'province_code' => 23, 'city_code' => null, 'area_code' => null, 'street_code' => null, 'parent_code' => 23],
+            ['name' => '黑河市', 'code' => 2311, 'level' => 2, 'province_code' => 23, 'city_code' => null, 'area_code' => null, 'street_code' => null, 'parent_code' => 23],
             // area level
-            ['name' => '密山市', 'code' => 230382, 'level' => 3, 'province_code' => 23, 'city_code' => 2303, 'area_code' => null, 'street_code' => null],
-            ['name' => '鸡东县', 'code' => 230321, 'level' => 3, 'province_code' => 23, 'city_code' => 2303, 'area_code' => null, 'street_code' => null],
+            ['name' => '密山市', 'code' => 230382, 'level' => 3, 'province_code' => 23, 'city_code' => 2303, 'area_code' => null, 'street_code' => null, 'parent_code' => 2303],
+            ['name' => '鸡东县', 'code' => 230321, 'level' => 3, 'province_code' => 23, 'city_code' => 2303, 'area_code' => null, 'street_code' => null, 'parent_code' => 2303],
             // street level
-            ['name' => '连珠山镇', 'code' => 230382101, 'level' => 4, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => null],
-            ['name' => '黑台镇', 'code' => 230382104, 'level' => 4, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => null],
+            ['name' => '连珠山镇', 'code' => 230382101, 'level' => 4, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => null, 'parent_code' => 230382],
+            ['name' => '黑台镇', 'code' => 230382104, 'level' => 4, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => null, 'parent_code' => 230382],
             // village level
-            ['name' => '永泉村委会', 'code' => 230382101205, 'level' => 5, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => 230382101],
-            ['name' => '连珠山村委会', 'code' => 230382101210, 'level' => 5, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => 230382101],
-            ['name' => '榆树村委会', 'code' => 230382104209, 'level' => 5, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => 230382104],
+            ['name' => '永泉村委会', 'code' => 230382101205, 'level' => 5, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => 230382101, 'parent_code' => 230382101],
+            ['name' => '连珠山村委会', 'code' => 230382101210, 'level' => 5, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => 230382101, 'parent_code' => 230382101],
+            ['name' => '榆树村委会', 'code' => 230382104209, 'level' => 5, 'province_code' => 23, 'city_code' => 2303, 'area_code' => 230382, 'street_code' => 230382104, 'parent_code' => 230382104],
         ]);
     }
 }
